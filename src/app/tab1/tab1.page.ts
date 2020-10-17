047d147897610912abed86edf3dda9865a3c49ec
Lucas Santiago <lsfdm09082002@gmail.com>
2020-10-17 13:55:33 -0300

    parte 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
-import { AlertController } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { HistoricoService } from '../servicos/historico.service';
+import { Historico } from '../models/Historico';
 
 @Component({
   selector: 'app-tab1',
@@ -15,15 +18,37 @@ export class Tab1Page {
 
   public content: HTMLElement;
   public imgLogo: HTMLElement;
+  public footer: HTMLElement;
 
   public leitura: string;
+  public link = false;
 
   constructor(
     private qrScanner: QRScanner,
-    public alertController: AlertController
-  ) { }
+    public alertController: AlertController,
+    public platform: Platform,
+    private screenOrientation: ScreenOrientation,
+    private cdRef:  ChangeDetectorRef,
+    private historicoService: HistoricoService
+  ) {
 
-  public lerQRCode() {
+    // set to landscape
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      this.content.style.opacity = '1';
+      this.imgLogo.style.opacity = '1';
+      this.footer.style.opacity = '1';
+
+      this.leitura = null;
+      this.link = false;
+
+      this.qrScanner.hide(); // hide camera preview
+      this.leitorQRCode.unsubscribe(); // stop scanning
+    });
+  }
+
+  public async lerQRCode() {
     // Optionally request the permission early
     this.qrScanner.prepare()
       .then((status: QRScannerStatus) => {
@@ -32,23 +57,41 @@ export class Tab1Page {
 
           this.content = document.getElementsByTagName('ion-content')[0] as HTMLElement;
           this.imgLogo = document.getElementById('logo') as HTMLElement;
+          this.footer = document.getElementById('footer') as HTMLElement;
 
           this.content.style.opacity = '0';
           this.imgLogo.style.opacity = '0';
+          this.footer.style.opacity = '0';
+
 
           this.qrScanner.show(); // show camera preview
           // start scanning
-          this.leitorQRCode = this.qrScanner.scan().subscribe((text: string) => {
-            
+          this.leitorQRCode = this.qrScanner.scan().subscribe(async (text: string) => {
+
             this.leitura = (text['result']) ? text['result'] : text;
 
             this.content.style.opacity = '1';
             this.imgLogo.style.opacity = '1';
+            this.footer.style.opacity = '1';
 
             this.qrScanner.hide(); // hide camera preview
             this.leitorQRCode.unsubscribe(); // stop scanning
 
-            this.presentAlert('LEITURA:', this.leitura);
+            // this.presentAlert('LEITURA:', this.leitura);
+            this.verificaLink(this.leitura);
+            this.cdRef.detectChanges();
+
+            const historico = new Historico();
+            historico.leitura = this.leitura;
+            historico.dataHora = new Date();
+
+            await this.historicoService.create(historico).then(resposta =>{
+              console.log(resposta);
+            }).catch(erro =>{
+              this.presentAlert('ERRO!', "Erro ao salvar no firebase.");
+              console.log('ERRO: ', erro);
+            });
+            
           });
 
         } else if (status.denied) {
@@ -64,11 +107,22 @@ export class Tab1Page {
 
   async presentAlert(titulo: string, mensagem: string) {
     const alert = await this.alertController.create({
-      header: titulo, 
+      header: titulo,
       message: mensagem,
       buttons: ['OK']
     });
 
     await alert.present();
+  }
+
+  public verificaLink(texto: string){
+
+    const inicio = texto.substring(0, 4);
+
+    if(inicio == 'www.' || inicio == "http"){
+      this.link = true;
+    }else{
+      this.link = false;
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDhQFkX4kAyNrFKVkT53BK98olw-WWgIZY",
+    authDomain: "leitorqrcodefirebase-bf53a.firebaseapp.com",
+    databaseURL: "https://leitorqrcodefirebase-bf53a.firebaseio.com",
+    projectId: "leitorqrcodefirebase-bf53a",
+    storageBucket: "leitorqrcodefirebase-bf53a.appspot.com",
+    messagingSenderId: "483680760787",
+    appId: "1:483680760787:web:1d3dd5e982c5fa1b02f526"
+  }
 };
 
 /*
